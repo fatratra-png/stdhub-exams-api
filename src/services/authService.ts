a9e97470685a4a17eb3d3096ee6118c03867b5b7
JulesTitran <titranj@gmail.com>
2026-08-22 20:07:52 +0300

    feat(security): make a re-usable password hash and compare file to secure login and sign in

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,10 @@
+import type { AuthRepository } from "../repositories/authRepository.ts";
+
 export class AuthService {
-    
+    constructor(private authRepository: AuthRepository) {}
+    async login(email: string, passwordPlain: string) {
+        const user = await this.authRepository.getUserAccount(email);
+        if (!user) throw new Error('Email ou mot de passe incorrect');
+        
+    }
 }
