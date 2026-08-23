b53b1a3900196132dd9e96586f156e9915789561
JulesTitran <titranj@gmail.com>
2026-08-23 18:26:12 +0300

    fix(questions): scope requireAuth/requireRole per-route, not router.use()

--- src/routes/questionRoute.ts
+++ src/routes/questionRoute.ts
@@ -5,9 +5,9 @@ import { requireAuth, requireRole } from "../middlewares/auth.ts";
 export const questionsRouter = Router();
 const questionController = new QuestionController();
 
-questionsRouter.use(requireAuth, requireRole("ADMIN"));
+const adminOnly = [requireAuth, requireRole("ADMIN")];
 
-questionsRouter.get("/exams/:id/questions", (req, res, next) => questionController.listForExam(req, res, next));
-questionsRouter.post("/exams/:id/questions", (req, res, next) => questionController.create(req, res, next));
-questionsRouter.put("/questions/:id", (req, res, next) => questionController.update(req, res, next));
-questionsRouter.delete("/questions/:id", (req, res, next) => questionController.remove(req, res, next));
+questionsRouter.get("/exams/:id/questions", ...adminOnly, (req, res, next) => questionController.listForExam(req, res, next));
+questionsRouter.post("/exams/:id/questions", ...adminOnly, (req, res, next) => questionController.create(req, res, next));
+questionsRouter.put("/questions/:id", ...adminOnly, (req, res, next) => questionController.update(req, res, next));
+questionsRouter.delete("/questions/:id", ...adminOnly, (req, res, next) => questionController.remove(req, res, next));
