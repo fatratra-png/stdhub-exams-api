d124c078863b252a5369158c067abb0380084a34
JulesTitran <titranj@gmail.com>
2026-08-24 15:06:37 +0300

    chore(docs): update env.example with CLIENT_URL environment variable strictly needed

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import type { NextFunction, Request, Response } from "express";
 import { HttpError } from "./errors/errors.ts";
 import { authRouter } from "./routes/authRoute.ts";
